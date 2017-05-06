9d0c6af434e03d179bf92ca5c312739cce032555
nayramartins <martinsg.nayra@gmail.com>
2017-05-06 00:02:55 -0300

    search form style

--- searchform.php
+++ searchform.php
@@ -1,5 +1,12 @@
-<div class="search--content">
-    <label for="search">Busca por palavra-chave</label>
-    <input type="text"/>
-    <img src="<?php bloginfo('url'); ?>/wp-content/themes/melhorespraticas/images/melhores_praticas-search.png" width="23" height="20" alt="">
+<div class="search-form">
+    <div class="search-form-top">
+        <p class="search-form-top_title color-grey subtitle">Busca por palavra-chave</p>
+        <div class="space"></div>
+        <span class="search-form-close"><img src="wp-content/themes/melhorespraticas/images/fechar_busca.png" alt=""></span>
+    </div>
+    <div class="search-form-bottom">
+        <input class="search-form-bottom_text" type="text"/>
+        <div class="space"></div>
+        <img class="search-form-bottom_icon" src="<?php bloginfo('url'); ?>/wp-content/themes/melhorespraticas/images/melhores_praticas-search.png" width="23" height="20" alt="">
+    </div>
 </div>
