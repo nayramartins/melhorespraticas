0afd247aec2285fc5c3e19cc0b12b96b1b9ff546
alinemorelliw <aline.morelli@sprinklr.com>
2017-05-06 23:19:29 -0300

    Mailing

--- banner-footer.php
+++ banner-footer.php
@@ -2,9 +2,6 @@
 	<div class="container">
 		<h3 class="full_banner--text lora-title">Fique Atualizado</h3>
 		<p class="text-content color-white">Inscreva-se e receba nossas notícias, programação e novidades direto na sua caixa de entrada.</p>
-		<form action="">
-			<input type="text" placeholder="digite seu e-mail" />
-		</form>
-		<a href="#" class="cta">cadastrar</a>
+		<?php echo do_shortcode('[mc4wp_form id="148"]'); ?>
 	</div>
 </section>
