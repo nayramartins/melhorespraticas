fb800464d3c93ad24928662740e7dcdbe891e5a2
alinemorelliw <aline.morelli@sprinklr.com>
2017-05-07 12:57:20 -0300

    Entrevistas

--- search.php
+++ search.php
@@ -31,23 +31,7 @@ get_header(); ?>
                             </li>
 			            <?php endwhile; ?>
                     </ul>
-                    <div class="search-right">
-                        <div class="advanced-search">
-                            <p class="lora-title advanced-title">Busca Avançada</p>
-                            <p class="subtitle color-grey">Palavra-chave</p>
-                            <input type="text" class="search-field"/>
-                            <p class="subtitle color-grey">Filtrar por</p>
-                            <ul class="search-filter">
-                                <li><a href="#" class="title subtitle">Matérias</a></li>
-                                <li><a href="#" class="title subtitle">Matérias</a></li>
-                                <li><a href="#" class="title subtitle">Matérias</a></li>
-                                <li><a href="#" class="title subtitle">Matérias</a></li>
-                            </ul>
-                        </div>
-                        <div class="merchandising_2">
-                            <p class="subtitle color-white">anuncie aqui</p>
-                        </div>
-                    </div>
+                    <?php get_sidebar(); ?>
                 </div>
             </section>
         <?php  else : ?>
